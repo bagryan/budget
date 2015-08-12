f3f2b9a0a7273524684479b38a16264bf30a4377
Yuriy Bagryanskiy <bagryan@xiag.ch>
2015-08-12 10:34:07 +0600

    layout

--- budget.js
+++ budget.js
@@ -8,6 +8,24 @@ angular.module('budgetApp', [])
                 "title": "Быстроном",
                 "amount": 1000,
                 "category": "Еда"
+            },
+            {
+                "date": "11.08.2015",
+                "title": "Бензин",
+                "amount": 1500,
+                "category": "Машина"
+            },
+            {
+                "date": "12.08.2015",
+                "title": "Быстроном",
+                "amount": 500,
+                "category": "Еда"
+            },
+            {
+                "date": "12.08.2015",
+                "title": "Спортзал",
+                "amount": 1000,
+                "category": "Здоровье"
             }
         ];
 
